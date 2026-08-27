995ef14ab7cf088bc689e7b1eb92a9dfa560d5e1
François HERBRETEAU <72505172+fherbreteau@users.noreply.github.com>
2026-08-27 02:56:46 +0200

    sonar: Fix minor issue linked to parametrized tests

--- src/test/java/io/github/fherbreteau/vodozemac/KeyValidatorTest.java
+++ src/test/java/io/github/fherbreteau/vodozemac/KeyValidatorTest.java
@@ -3,8 +3,13 @@ package io.github.fherbreteau.vodozemac;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.stream.Stream;
+
 import io.github.fherbreteau.vodozemac.exception.KeyException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class KeyValidatorTest {
 
@@ -22,25 +27,17 @@ class KeyValidatorTest {
                 .hasMessage("Encrypted Key must be 256-bit (32-byte)");
     }
 
-    @Test
-    void testValidateKeyTooShort() {
-        byte[] key = new byte[16];
-        assertThatThrownBy(() -> KeyValidator.validateEncryptionKey(key))
-                .isInstanceOf(KeyException.class)
-                .hasMessage("Encrypted Key must be 256-bit (32-byte)");
+    public static Stream<Arguments> invalidSize() {
+        return Stream.of(
+            Arguments.of(new byte[16]),
+            Arguments.of(new byte[64]),
+            Arguments.of(new byte[0])
+        );
     }
 
-    @Test
-    void testValidateKeyTooLong() {
-        byte[] key = new byte[64];
-        assertThatThrownBy(() -> KeyValidator.validateEncryptionKey(key))
-                .isInstanceOf(KeyException.class)
-                .hasMessage("Encrypted Key must be 256-bit (32-byte)");
-    }
-
-    @Test
-    void testValidateEmptyKey() {
-        byte[] key = new byte[0];
+    @ParameterizedTest
+    @MethodSource("invalidSize")
+    void testValidateKeyWithInvalidSize(byte[] key) {
         assertThatThrownBy(() -> KeyValidator.validateEncryptionKey(key))
                 .isInstanceOf(KeyException.class)
                 .hasMessage("Encrypted Key must be 256-bit (32-byte)");
