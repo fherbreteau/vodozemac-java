bb99ef2ebcf76c9b36d42c700f0bc45a2406ca2a
François HERBRETEAU <72505172+fherbreteau@users.noreply.github.com>
2026-09-07 03:38:51 +0200

    test(java): run sample programs as part of the test suite

---
+++ src/test/java/io/github/fherbreteau/vodozemac/examples/SamplesTest.java
@@ -0,0 +1,31 @@
+package io.github.fherbreteau.vodozemac.examples;
+
+import org.junit.jupiter.api.Test;
+
+class SamplesTest {
+
+    @Test
+    void runSampleOlm() {
+        SampleOlm.main(new String[0]);
+    }
+
+    @Test
+    void runSampleMegolm() {
+        SampleMegolm.main(new String[0]);
+    }
+
+    @Test
+    void runSampleSas() {
+        SampleSas.main(new String[0]);
+    }
+
+    @Test
+    void runSampleEcies() {
+        SampleEcies.main(new String[0]);
+    }
+
+    @Test
+    void runSampleBackup() {
+        SampleBackup.main(new String[0]);
+    }
+}
